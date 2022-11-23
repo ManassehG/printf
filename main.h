ab299379bb4b1e1b5940e49d50360250733aed20
ManassehG <manasseh65mwas25@gmail.com>
2022-11-23 08:08:22 -0800

    Conversion specifier

--- main.h
+++ main.h
@@ -16,4 +16,15 @@ unsigned int convert_s(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
+/**
+ * struct converter_s - A new type defining a converter struct.
+ * @specifier: A character representing a conversion specifier.
+ * @func: A pointer to a conversion function corresponding to specifier.
+ */
+typedef struct converter_s
+{
+	unsigned char specifier;
+	unsigned int (*func)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+} converter_t;
 #endif /* MAIN_H */
