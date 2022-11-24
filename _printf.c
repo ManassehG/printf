c22a16c2fcb219ff8a4ea05774b5e1845c79acf2
NjeriKiarie <kcarolnjeri@gmail.com>
2022-11-24 13:49:34 -0800

    P

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, wid, prec, len, buffer_t = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list args;
 	char buffer[BUFF_SIZE];
 
@@ -20,32 +20,30 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buffer_t++] = format[i];
-			if (buffer_t == BUFF_SIZE)
-				print_buffer(buffer, &buffer_t);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buffer_t);
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			wid = get_width(format, &i, args);
-			prec = get_precision(format, &i, args);
-			len = get_size(format, &i);
+			width = get_width(format, &i, args);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, args, buffer,
-					flags, wid, prec, len);
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
-	print_buffer(buffer, &buffer_t);
+	print_buffer(buffer, &buff_ind);
 	va_end(args);
 	return (printed_chars);
 }
-
-
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
@@ -53,9 +51,9 @@ int _printf(const char *format, ...)
  * @buff_ind: Index at which to add next char, represents the length.
  */
 
-void print_buffer(char buffer[], int *buffer_t)
+void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buffer_t > 0)
-		write(1, &buffer[0], *buffer_t);
-	*buffer_t = 0;
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
